caa49faa26771a84cbdadcfe1cdba39c187ede60
memeanji <kllee000@naver.com>
2026-05-19 17:28:05 +0900

    chore: add v2 entrypoint placeholder

---
+++ src/open-campaign-v2.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * v2 entrypoint placeholder.
+ *
+ * Restore the previous open-campaign workflow here, then move stable helpers into
+ * ./lib and selectors into ./selectors as the implementation matures.
+ */
+
+async function runOpenCampaignV2() {
+  throw new Error('open-campaign v2 is not implemented yet. Restore the v1 source before adding v2 behavior.');
+}
+
+module.exports = {
+  runOpenCampaignV2,
+};
