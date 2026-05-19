33756e6bff50b7f7099d8264a6d7ec04e16b47a1
memeanji <kllee000@naver.com>
2026-05-19 17:28:08 +0900

    chore: add meta campaign selector registry

---
+++ src/selectors/meta-campaign.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const META_CAMPAIGN_SELECTORS = {
+  creativeSettingsButtonClass: 'x78zum5 xdt5ytf x2lwn1j xeuugli xkh2ocl',
+  imageAdWrapperClass: 'x6s0dn4 x1q0g3np xozqiw3 x2lwn1j x1iyjqo2 xs83m0k x1xsc7gk x78zum5 xeuugli',
+  imageAdLabelClass: 'x1vvvo52 x1fvot60 xo1l8bm xxio538 xbsr9hj xq9mrsl x1mzt3pk x1vvkbs x13faqbe xeuugli x1iyjqo2',
+  imageAdText: '이미지 광고',
+  surfaceWrapper: 'span[data-surface-wrapper="1"]',
+};
+
+module.exports = {
+  META_CAMPAIGN_SELECTORS,
+};
